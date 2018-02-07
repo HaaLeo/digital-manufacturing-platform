881412d38aaf51d1e6ac07d0e9d75c061a660919
Tasos Kalogeropoulos <tasoskalogero@gmail.com>
2018-02-07 11:26:19 +0100

    fileupload added code to css file and html from component

--- angular-app/src/app/fileupload/fileupload.component.ts
+++ angular-app/src/app/fileupload/fileupload.component.ts
@@ -5,84 +5,87 @@ import masterAssetBigchain from './bigchain-post/masterAssetBigchain.js';
 
 @Component({
   selector: 'fileupload',
-  template: `
+  templateUrl: './fileupload.component.html',
+  styleUrls: ['./fileupload.component.css'],
+  
+  // template: `
   
     
-    <div class="file-drop-container">
-      <div ng2FileDrop class="custom-component-drop-zone"
+  //   <div class="file-drop-container">
+  //     <div ng2FileDrop class="custom-component-drop-zone"
   
-       [ng2FileDropMaximumSizeBytes]="maximumFileSizeInBytes"
-       [ng2FileDropSupportedFileTypes]="supportedFileTypes"
+  //      [ng2FileDropMaximumSizeBytes]="maximumFileSizeInBytes"
+  //      [ng2FileDropSupportedFileTypes]="supportedFileTypes"
   
-       (ng2FileDropHoverStart)="dragFileOverStart()" 
-       (ng2FileDropHoverEnd)="dragFileOverEnd()"
-       (ng2FileDropFileAccepted)="dragFileAccepted($event)" 
-       (ng2FileDropFileRejected)="dragFileRejected($event)">
+  //      (ng2FileDropHoverStart)="dragFileOverStart()" 
+  //      (ng2FileDropHoverEnd)="dragFileOverEnd()"
+  //      (ng2FileDropFileAccepted)="dragFileAccepted($event)" 
+  //      (ng2FileDropFileRejected)="dragFileRejected($event)">
         
-        <div *ngIf="!imageShown">
-             <p class="instructions">Drag and Drop your blueprint.</p>
-        </div>
+  //       <div *ngIf="!imageShown">
+  //            <p class="instructions">Drag and Drop your blueprint.</p>
+  //       </div>
         
-        <div *ngIf="imageShown" class=image-container>
-            <p class="instructions">Thank you for your design!</p>
-        </div>
+  //       <div *ngIf="imageShown" class=image-container>
+  //           <p class="instructions">Thank you for your design!</p>
+  //       </div>
         
-      </div>
-    </div>
+  //     </div>
+  //   </div>
   
-  `,
+  // `,
   
-  styles: [`
+  // styles: [`
   
-    .file-drop-container {
-      margin: 30 auto;
-      padding-top: 15px;
-    }
+  //   .file-drop-container {
+  //     margin: 30 auto;
+  //     padding-top: 15px;
+  //   }
   
-    .custom-component-drop-zone {
-      margin: 0 auto;
+  //   .custom-component-drop-zone {
+  //     margin: 0 auto;
 
-      border-style: dashed;
-      border-width: 2px;
-      border-radius: 30px;
-      border-color: #979797;
+  //     border-style: dashed;
+  //     border-width: 2px;
+  //     border-radius: 30px;
+  //     border-color: #979797;
   
-      width: 300px;
-      height: 300px;
+  //     width: 300px;
+  //     height: 300px;
   
-      text-align: center;
-    }
+  //     text-align: center;
+  //   }
     
-    .instructions {
-      position: relative;
-      color: #979797;
+  //   .instructions {
+  //     position: relative;
+  //     color: #979797;
   
-      text-align: center;
+  //     text-align: center;
   
-      font-size: 12px;
+  //     font-size: 12px;
   
-      top: 40px;
-      width: 100%;
-    }
+  //     top: 40px;
+  //     width: 100%;
+  //   }
     
-    .image-container {
+  //   .image-container {
 
-      width: 100%;
-      height: 100%;
+  //     width: 100%;
+  //     height: 100%;
   
-      overflow: hidden;
-      position: relative;
+  //     overflow: hidden;
+  //     position: relative;
   
-      border-radius: 30px;
-    }
-    .request-image {
+  //     border-radius: 30px;
+  //   }
+  //   .request-image {
 
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
+  //     width: 100%;
+  //     height: 100%;
+  //     object-fit: cover;
+  //   }
   
-  `]
+  // `]
 })
 export class FileuploadComponent {
 
