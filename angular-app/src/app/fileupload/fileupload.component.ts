e315b8023c5316e8ba9ec83232b3b19a37ede9d5
hanisch-privat <HanischLeo@freenet.de>
2018-06-21 17:31:24 +0200

    * Removed unused file-upload components * Created methods to store, retrive keys from bigchaindb

--- angular-app/src/app/fileupload/fileupload.component.ts
+++ angular-app/src/app/fileupload/fileupload.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ng2FileDropAcceptedFile, Ng2FileDropRejectedFile }  from 'ng2-file-drop';
-import masterAssetBigchain from './bigchain-post/masterAssetBigchain.js';
+import {postKeyToBcDB, getAssetFromBcDB} from './bigchain-post/ipfsKeyAssetBigchain.js';
 import generateCS from './bigchain-post/ChecksumGenerator.js';
 
 
@@ -12,7 +12,7 @@ import generateCS from './bigchain-post/ChecksumGenerator.js';
 export class FileuploadComponent {
 
   //Maximum file size in bytes
-  
+
   //Supported File Types
   private txid: string;
   private imageShown: boolean = false;
@@ -25,18 +25,18 @@ export class FileuploadComponent {
   private dragFileOverStart() {
     console.log("File being dragged has moved into the drop region");
   }
- 
+
   // File being dragged has moved out of the drop region
   private dragFileOverEnd() {
     console.log("File being dragged has moved out of the drop region");
   }
- 
+
   // File being dragged has been dropped and is valid
   private dragFileAccepted(acceptedFile: Ng2FileDropAcceptedFile) {
     console.log("File being dragged has been dropped and is valid");
-    
+
     // Load the image in
-     
+
      this.acceptedFile = acceptedFile.file;
      console.log("test: " + acceptedFile.file.name)
      this.currentFileName = acceptedFile.file.name;
@@ -50,17 +50,20 @@ export class FileuploadComponent {
       // Read in the file
     // masterAssetBigchain(acceptedFile.file)
     // .then(txid => {
-    //   console.log("[dragFileAccepted]", txid);  
+    //   console.log("[dragFileAccepted]", txid);
     // });
   }
- 
+
  getChecksum() {
    return this.checksum;
  }
-  async postBCDB(price, description, ownerID) {
-    return await masterAssetBigchain(this.acceptedFile, price, description, ownerID)
+  async postBCDB(key, description, ownerID) {
+    return await postKeyToBcDB(key, description, ownerID)
   }
 
+  async getBCDB(txId) {
+    return await getAssetFromBcDB(txId);
+  }
   // File being dragged has been dropped and has been rejected
   private dragFileRejected(rejectedFile: Ng2FileDropRejectedFile) {
     console.log("File being dragged has been dropped and has been rejected");
