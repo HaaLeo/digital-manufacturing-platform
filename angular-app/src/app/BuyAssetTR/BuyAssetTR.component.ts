6b84f5b1925e248bf2169f815e5fc00bdaad08bb
MaximilianJohannesObpacher <m.obpacher@anfe.ma>
2018-07-04 18:19:49 +0200

    Mocking and uploading quality report added

--- angular-app/src/app/BuyAssetTR/BuyAssetTR.component.ts
+++ angular-app/src/app/BuyAssetTR/BuyAssetTR.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 import { BuyAssetTRService } from './BuyAssetTR.service';
 import {Printer, PrintingJob, QualityReport, Stakeholder} from "../org.usecase.printer";
 import {QualityReportService} from "../QualityReport/QualityReport.service";
+import {PrinterService} from "../Printer/Printer.service";
 
 declare function require(name:string);
 let sha512 = require('js-sha512');
@@ -11,7 +12,7 @@ let sha512 = require('js-sha512');
 	selector: 'app-BuyAssetTR',
 	templateUrl: './BuyAssetTR.component.html',
 	styleUrls: ['./BuyAssetTR.component.css'],
-  	providers: [BuyAssetTRService, QualityReportService]
+  	providers: [BuyAssetTRService, QualityReportService, PrinterService]
 })
 
 export class BuyAssetTRComponent {
@@ -23,17 +24,19 @@ export class BuyAssetTRComponent {
 	private progressMessage;
     private successMessage;
 	private allPrintingJobs;
+	private allPrinters;
+	private printer;
 
 	private printingJobCurrent;
 	private confirmTransactionObj;
 	private qualityReportObj;
     private transactionID;
 	private selectedJob;
-    private qualityProperties;
     private evaluateReportObj;
     private current_db_id;
 
-	constructor(private serviceTransaction: BuyAssetTRService, fb: FormBuilder, private serviceQualityReport: QualityReportService) {
+	constructor(private serviceTransaction: BuyAssetTRService, fb: FormBuilder,
+                private serviceQualityReport: QualityReportService, private servicePrinter: PrinterService) {
 		this.myForm = fb.group({
             printingJobID: this.printingJobID,
 	  });
@@ -46,6 +49,7 @@ export class BuyAssetTRComponent {
 				this.transactionFrom  = true;
 		});
 		this.loadAllQualityReports();
+        this.loadAllPrinters();
 	  }
 
 	// Get all PrintingJobs
@@ -88,7 +92,8 @@ export class BuyAssetTRComponent {
                 result.forEach(qualityReport => {
                         tempList.push(qualityReport);
                 });
-                if (0 < tempList.length) {
+                debugger;
+                if ( 0 < tempList.length) {
                     this.current_db_id = tempList[tempList.length - 1];
                 } else {
                     this.current_db_id = 0;
@@ -110,16 +115,38 @@ export class BuyAssetTRComponent {
             });
     }
 
-
-    /**
-    TODO uploadQualityReport to ipfs. (as a json.) read this and pass it into the evaluateReport Method.
-     This might be implemented in an own transaction. Do not forget to upload QualityRequirement properly aswell.
-    **/
+    loadAllPrinters(): Promise<any> {
+        const tempList = [];
+        return this.servicePrinter.getAllPrinters()
+            .toPromise()
+            .then((result) => {
+                this.errorMessage = null;
+                result.forEach(printer => {
+                    tempList.push(printer);
+                });
+               this.allPrinters = tempList;
+            })
+            .catch((error) => {
+                if (error == 'Server error'){
+                    this.progressMessage = null;
+                    this.errorMessage = 'Could not connect to REST server. Please check your configuration details';
+                }
+                else if (error == '404 - Not Found'){
+                    this.progressMessage = null;
+                    this.errorMessage = '404 - Could not find API route. Please check your available APIs.';
+                }
+                else{
+                    this.progressMessage = null;
+                    this.errorMessage = error;
+                }
+            });
+    }
 
   	evaluateReport(form: any){
+  	    debugger;
         this.evaluateReportObj = {
             $class: "org.usecase.printer.EvaluateReport",
-            "printingJob": this.printingJobCurrent, // includes Quality Requirement and BlueprintMaster
+            "printingJob": this.printingJobCurrent.printingJobID, // includes Quality Requirement and BlueprintMaster
             "customer": this.printingJobCurrent.buyer,
             "qualityReport": this.qualityReportObj,
         };
@@ -149,39 +176,78 @@ export class BuyAssetTRComponent {
             });
     }
 
+    uploadQualityReport(form: any){
+
+        console.log(this.allPrintingJobs);
+        for (const printingJob of this.allPrintingJobs) {
+            if (printingJob.printingJobID == this.printingJobID.value) {
+                this.printingJobCurrent = printingJob;
+                debugger;
+            }
+        }
+        console.log(this.allPrinters);
+        for (const printer of this.allPrinters) {
+            debugger;
+            if (printer.stakeholderID == this.printingJobCurrent.printer.split("#")[1]) {
+                this.printer = printer;
+                debugger;
+            }
+        }
+        debugger;
+        let qualityCriteria = {
+            "peakPressure":Math.random()*3000,
+            "peakTemperature": Math.random()*800
+        };
+        this.current_db_id =(this.allPrinters).length;
+        debugger;
+        this.qualityReportObj = {
+            "$class": "org.usecase.printer.QualityReport",
+            "qualityReportID":"QREP_" + this.current_db_id,
+            "txID": sha512(JSON.stringify(qualityCriteria)),
+            "databaseHash": sha512(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 255)),
+            "owner": this.printer.printerManufacturer,
+            "printingJob": "resource:org.usecase.printer.PrintingJob#"+this.printingJobCurrent.printingJobID,
+        };
+        debugger;
+        return this.serviceQualityReport.addAsset(this.qualityReportObj)
+            .toPromise()
+            .then(() => {
+                debugger;
+                this.errorMessage = null;
+                this.progressMessage = null;
+                this.successMessage = 'QualityReport added successfully.';
+            })
+            .catch((error) => {
+                if(error == 'Server error'){
+                    this.progressMessage = null;
+                    this.errorMessage = "Could not connect to REST server. Please check your configuration details";
+                }
+                else{
+                    this.progressMessage = null;
+                    this.errorMessage = error;
+                }
+            });
+
+    }
+
   	execute(form: any){
 	    debugger;
 		this.progressMessage = 'Please wait... ';
   	console.log(this.allPrintingJobs);
 		for (const printingJob of this.allPrintingJobs) {
-			if (printingJob.printingJobID == this.printingJobID.value) {
-				this.printingJobCurrent = printingJob;
-				debugger;
-			}
-		}
+            if (printingJob.printingJobID == this.printingJobID.value) {
+                this.printingJobCurrent = printingJob;
+                debugger;
+            }
+        }
+
     	this.confirmTransactionObj = {
 	      "$class": "org.usecase.printer.ConfirmTransaction",
 	      "printingJob": "resource:org.usecase.printer.PrintingJob#"+this.printingJobCurrent.printingJobID
 	    };
-	    return this.serviceTransaction.printBlueprint(this.confirmTransactionObj)
+	   return this.serviceTransaction.printBlueprint(this.confirmTransactionObj)
 	    .toPromise()
 	    .then((result) => {
-	        let qualityCriteria = {
-	            "peakPressure":Math.random()*3000,
-                "peakTemperature": Math.random()*800
-            };
-	        this.current_db_id ++;
-	        this.qualityReportObj = {
-                "$class": "org.usecase.printer.QualityReport",
-                "qualityReportID":"QREP_" + this.current_db_id,
-                "txID": sha512(JSON.stringify(qualityCriteria)),
-                "databaseHash": sha512(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 255)),
-                "owner": this.printingJobCurrent.printer.printerManufacturer.stakeholderID,
-                "printingJob": this.printingJobCurrent.printingJobID,
-	        };
-	        this.serviceQualityReport.addAsset(this.qualityReportObj);
-            debugger;
-            this.evaluateReport(form);
 	    	this.selectedJob = null;
 	    	this.errorMessage = null;
 	    	this.progressMessage = null;
