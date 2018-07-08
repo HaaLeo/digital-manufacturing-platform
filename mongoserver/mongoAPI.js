f07c556a9d1c83bce37cf16f2c95f1e64485ae27
Aidan Beale <aidanbeale@gmail.com>
2018-07-08 14:00:35 +0200

    POST and GET data functional

--- mongoserver/mongoAPI.js
+++ mongoserver/mongoAPI.js
@@ -5,17 +5,17 @@ var MongoClient = require('mongodb').MongoClient
 var request = require("request");
 
 var url = 'mongodb://localhost:27017';
-var db;
+var dbase;
 
 MongoClient.connect(url, function(err, db) {   //here db is the client obj
     if (err) {
       throw err;
     } else {
-    var dbase = db.db("mydb"); //here
+    dbase = db.db("mydb"); //here
     dbase.createCollection("QualityReports", function(err, res) {
         if (err) throw err;
         console.log("QualityReports collection created!");
-        db.close();   //close method has also been moved to client obj
+        //db.close();   //close method has also been moved to client obj
     });
 
     var port = process.env.PORT || 3004;
@@ -34,20 +34,41 @@ app.post('/addData', function(req, res) {
     var temperature = req.body.temperature;
     var pressure = req.body.pressure;
 
+    console.log("DATA: hashID (" + hashID + "), temperature (" + temperature + "), pressure (" + pressure + ")");
+
     insert = {
         'hashID': hashID,
         'temperature': temperature,
         'pressure': pressure
     }
 
-    db.collection('QualityReports').insert(insert, function(err, doc) {
+    dbase.collection('QualityReports').insert(insert, function(err, doc) {
         if (err) {
             console.log("Error adding data: " + err);
             throw err;
         } else {
-            res.send()
-            console.log('Added data');
+            res.send(doc)
+            console.log('Added data + ' + doc);
         }
     });
+});
 
+app.get('/getData/:hashID', function(req, res) {
+  console.log("Quality Report Data being requested for hash: " + req.params.hashID);
+  var hashID = req.params.hashID;
+  dbase.collection('QualityReports').find({"hashID":hashID}).toArray(function(err, result) {
+    if (err) {
+        console.log("Error retrieving data: " + err);
+        throw err;
+    } else {
+      console.log("RESULT: " + result);
+      //pipe items
+      var completeString = "";
+      result.forEach(function(value) {
+        completeString += JSON.stringify(value);
+        completeString += "|";
+      });
+      res.send(completeString);
+    }
+  });
 });
