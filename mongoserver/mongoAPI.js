6ee117119ab641ceddbd0096341ea7b043d8f013
Aidan Beale <aidanbeale@gmail.com>
2018-07-08 14:05:16 +0200

    Fixed formatting

--- mongoserver/mongoAPI.js
+++ mongoserver/mongoAPI.js
@@ -7,17 +7,17 @@ var request = require("request");
 var url = 'mongodb://localhost:27017';
 var dbase;
 
-MongoClient.connect(url, function(err, db) {   //here db is the client obj
-    if (err) {
-      throw err;
-    } else {
-    dbase = db.db("mydb"); //here
+MongoClient.connect(url, function(err, db) {
+  if (err) {
+   throw err;
+  } else {
+    dbase = db.db("mydb");
     dbase.createCollection("QualityReports", function(err, res) {
         if (err) throw err;
-        console.log("QualityReports collection created!");
-        //db.close();   //close method has also been moved to client obj
+        console.log("QualityReports collection created");
     });
 
+    //Set up API Server
     var port = process.env.PORT || 3004;
       app.listen(port, function () {
     });
@@ -29,40 +29,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
 
 app.post('/addData', function(req, res) {
-    console.log('POST to /addData');
-    var hashID = req.body.hashID;
-    var temperature = req.body.temperature;
-    var pressure = req.body.pressure;
+  console.log('POST to /addData');
+  var hashID = req.body.hashID;
+  var temperature = req.body.temperature;
+  var pressure = req.body.pressure;
 
-    console.log("DATA: hashID (" + hashID + "), temperature (" + temperature + "), pressure (" + pressure + ")");
+  console.log("DATA: hashID (" + hashID + "), temperature (" + temperature + "), pressure (" + pressure + ")");
 
-    insert = {
-        'hashID': hashID,
-        'temperature': temperature,
-        'pressure': pressure
+  insert = {
+    'hashID': hashID,
+    'temperature': temperature,
+    'pressure': pressure
+  }
+
+  dbase.collection('QualityReports').insert(insert, function(err, doc) {
+    if (err) {
+      console.log("Error adding data: " + err);
+     throw err;
+    } else {
+     res.send(doc);
+     console.log('Added data of hash value: ' + hashID);
     }
-
-    dbase.collection('QualityReports').insert(insert, function(err, doc) {
-        if (err) {
-            console.log("Error adding data: " + err);
-            throw err;
-        } else {
-            res.send(doc)
-            console.log('Added data + ' + doc);
-        }
-    });
+  });
 });
 
 app.get('/getData/:hashID', function(req, res) {
   console.log("Quality Report Data being requested for hash: " + req.params.hashID);
+
   var hashID = req.params.hashID;
+
   dbase.collection('QualityReports').find({"hashID":hashID}).toArray(function(err, result) {
     if (err) {
         console.log("Error retrieving data: " + err);
         throw err;
     } else {
-      console.log("RESULT: " + result);
-      //pipe items
       var completeString = "";
       result.forEach(function(value) {
         completeString += JSON.stringify(value);
